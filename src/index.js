4ad78124e31f8654a5ca54a3db191c9fd482e338
Paul Onchera <hesbonpoisson@gmail.com>
2020-07-22 11:07:29 +0300

    added a shopping cart icon and navigation bar

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavHeader from './components/Header/index';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavHeader />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
