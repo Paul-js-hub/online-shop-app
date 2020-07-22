e8bb565b767606ad070b0eb60b03a63fcd5d667f
Paul Onchera <hesbonpoisson@gmail.com>
2020-07-22 18:04:23 +0300

    added images to the products component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavHeader from './components/Header/index';
+import Products from './components/Products/index'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
+      <NavHeader />
+      <Products />
     </div>
   );
 }
